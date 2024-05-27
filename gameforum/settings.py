be163049f244f4e3092e04129e660d9dec1ab9f8
frank-ezenwanne <efrank272@gmail.com>
2024-05-27 16:14:38 +0100

    changes

--- gameforum/settings.py
+++ gameforum/settings.py
@@ -81,12 +81,26 @@ WSGI_APPLICATION = 'gameforum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),  
+        'PORT': os.getenv('PGPORT'),       
     }
 }
+
 
 
 # Password validation
